fb1e801af1bdcbfdd3fe43f7c2224a8e8fc17f34
Idooru <shere1765@gmail.com>
2023-08-13 00:35:16 +0900

    에러 내용을 리다이렉트

--- js/error.js
+++ js/error.js
@@ -1,23 +1,14 @@
 export const error = (errorCase) => {
-  const contentItemsEle = document.querySelector(".content-items");
-  const errorEle = document.querySelector(".error");
-  const errorContentEle = document.createElement("p");
-  const buttons = document.createElement("button");
-
-  contentItemsEle.style.display = "none";
-  buttons.className = "error-page-button";
-  errorEle.style.display = "block";
-
   switch (errorCase) {
     case "over flow length":
-      errorContentEle.innerText =
-        "책 제목의 길이가 25글자를 넘지 않게 해주세요!";
-      buttons.innerText = "수정";
-      errorEle.appendChild(errorContentEle);
-      errorEle.appendChild(buttons);
-      break;
+      const redirectUrl = "../error.html?error-content-message";
+      const contentMessage = encodeURIComponent(
+        "책 제목의 길이가 25글자를 넘지 않게 해주세요!"
+      );
+      const buttonMessage = encodeURIComponent("수정");
+
+      window.location.href = `${redirectUrl}=${contentMessage}&button-message=${buttonMessage}`;
     case "not found name":
       errorContentEle.innerText = "첵 재목이 존재하지 않습니다!";
-      break;
   }
 };

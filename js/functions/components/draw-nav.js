8edbe54f09615c97bca3828665e50b930ff1db86
Idooru <shere1765@gmail.com>
2023-08-13 19:39:42 +0900

    책 추가 버튼에 이벤트 부여

--- js/functions/components/draw-nav.js
+++ js/functions/components/draw-nav.js
@@ -1,7 +1,10 @@
 import { searchEvent } from "../events/search-event.js";
+import { addEvent } from "../events/add-event.js";
 
 export const drawNav = () => {
   const searchButtonEle = document.querySelector("#search-button");
+  const addButtonEle = document.querySelector("#add-button");
 
   searchButtonEle.addEventListener("click", searchEvent);
+  addButtonEle.addEventListener("click", addEvent);
 };

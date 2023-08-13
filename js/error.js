152b6a99a33ecb2cd79daa3d730e9f9d5da6122c
Idooru <shere1765@gmail.com>
2023-08-13 17:21:28 +0900

    책 제목이 없을 때 발생할 에러 추가

--- js/error.js
+++ js/error.js
@@ -1,14 +1,20 @@
 export const error = (errorCase) => {
+  const redirectUrl = "../error.html?error-content-message";
+  let contentMessage;
+  let buttonMessage;
+
   switch (errorCase) {
     case "over flow length":
-      const redirectUrl = "../error.html?error-content-message";
-      const contentMessage = encodeURIComponent(
+      contentMessage = encodeURIComponent(
         "책 제목의 길이가 25글자를 넘지 않게 해주세요!"
       );
-      const buttonMessage = encodeURIComponent("수정");
+      buttonMessage = encodeURIComponent("수정");
 
       window.location.href = `${redirectUrl}=${contentMessage}&button-message=${buttonMessage}`;
     case "not found name":
-      errorContentEle.innerText = "첵 재목이 존재하지 않습니다!";
+      contentMessage = encodeURIComponent("해당 책 제목은 존재하지 않습니다!");
+      buttonMessage = encodeURIComponent("수정");
+
+      window.location.href = `${redirectUrl}=${contentMessage}&button-message=${buttonMessage}`;
   }
 };

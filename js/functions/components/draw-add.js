b8da78f71a77a66f074ebe32c29342f1cc344268
Idooru <shere1765@gmail.com>
2023-08-14 17:15:42 +0900

    책을 추가 하는 버튼의 동작

--- js/functions/components/draw-add.js
+++ js/functions/components/draw-add.js
@@ -1,5 +1,25 @@
 import { drawInfo } from "./draw-info.js";
 
 export const drawAdd = () => {
+  const submitButtonEle = document.querySelector("#submit-button");
+
+  submitButtonEle.addEventListener("click", (event) => {
+    event.preventDefault();
+    const inputs = document.querySelectorAll("#input-box input");
+    const [subject, author, publisher, date, price, summary, photo] = inputs;
+
+    let newBook = {
+      subject: subject.value,
+      author: author.value,
+      publisher: publisher.value,
+      date: date.value,
+      price: price.value,
+      summary: summary.value,
+      photo: photo.value,
+    };
+
+    window.location.href = "/";
+  });
+
   drawInfo("책을 추가합니다!");
 };
